388b599b5ef064a37a019f1cfcaab408598a77e1
Afaq Virk <afaq.virk@shopify.com>
2025-01-17 12:59:35 -0500

    added items

--- afmenus.gemspec
+++ afmenus.gemspec
@@ -8,17 +8,17 @@ Gem::Specification.new do |spec|
   spec.authors = ["Afaq Virk"]
   spec.email = ["afaq.virk@shopify.com"]
 
-  spec.summary = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description = "TODO: Write a longer description or delete this line."
-  spec.homepage = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{A simple Ruby gem for interactive terminal menus.}
+  spec.description   = %q{Enables developers to create navigable, interactive menus in the terminal using arrow keys and Enter.}
+  spec.homepage      = "https://github.com/afaqmvirk/afmenus"
   spec.license = "MIT"
   spec.required_ruby_version = ">= 3.1.0"
 
   spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/afaqmvirk/afmenus"
+  spec.metadata["changelog_uri"] = "https://github.com/afaqmvirk/afmenus/CHANGELOG.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -29,7 +29,7 @@ Gem::Specification.new do |spec|
         f.start_with?(*%w[bin/ test/ spec/ features/ .git appveyor Gemfile])
     end
   end
-  spec.bindir = "exe"
+  spec.bindir = ["exe"]
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
--- lib/afmenus.rb
+++ lib/afmenus.rb
@@ -1,8 +1,66 @@
 # frozen_string_literal: true
 
+# lib/afmenus.rb
+
+require "io/console"  # For capturing user input
 require_relative "afmenus/version"
 
 module Afmenus
-  class Error < StandardError; end
-  # Your code goes here...
+  # Clear the console
+  def self.clear_console
+    print "\e[H\e[2J"
+  end
+
+  # Print the menu with the current selection highlighted
+  def self.print_menu(menu_items, current_row, current_col)
+    clear_console
+    menu_items.each_with_index do |row_items, row|
+      row_items.each_with_index do |item, col|
+        if row == current_row && col == current_col
+          print "\e[7m#{item.center(item.length + 2)}\e[0m"
+        else
+          print "#{item.center(item.length + 2)}"
+        end
+        print " | " unless col == row_items.length - 1
+      end
+      puts
+    end
+    puts "\nUse Arrow Keys to move, Enter to select, Ctrl+C to exit."
+  end
+
+  # Prompt the user with the menu
+  def self.prompt(menu_items)
+    current_row = 0
+    current_col = 0
+
+    print_menu(menu_items, current_row, current_col)
+
+    loop do
+      char = STDIN.getch
+
+      case char
+      when "\e"   # Escape sequence
+        next_char = STDIN.getch
+        if next_char == "["
+          arrow_key = STDIN.getch
+          case arrow_key
+          when "A" # Up arrow
+            current_row -= 1 if current_row > 0
+          when "B" # Down arrow
+            current_row += 1 if current_row < menu_items.length - 1
+          when "C" # Right arrow
+            current_col += 1 if current_col < menu_items[current_row].length - 1
+          when "D" # Left arrow
+            current_col -= 1 if current_col > 0
+          end
+        end
+      when "\r", "\n" # Enter key
+        return menu_items[current_row][current_col]
+      when "\u0003"    # Ctrl+C
+        return nil
+      end
+
+      print_menu(menu_items, current_row, current_col)
+    end
+  end
 end
